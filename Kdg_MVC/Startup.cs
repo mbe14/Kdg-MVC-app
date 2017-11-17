1348ca98132bbf4d5d7c2c875d2914b3ba284d50
Mihai Benegui <mihai.benegui@intercode.ro>
2017-11-17 10:15:37 +0200

    Add project files.

---
+++ Kdg_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kdg_MVC.Startup))]
+namespace Kdg_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
