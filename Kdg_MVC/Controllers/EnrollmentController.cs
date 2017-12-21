3db98830257671592f5be8da13d4a41948994478
Mihai <mbenegui@gmail.com>
2017-12-21 15:37:31 +0200

    Fixed property names.

--- Kdg_MVC/Controllers/EnrollmentController.cs
+++ Kdg_MVC/Controllers/EnrollmentController.cs
@@ -49,7 +49,7 @@ namespace Kdg_MVC.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "EnrollmentID,InstructorID,GroupID,ChildID,Grade")] Enrollment enrollment)
+        public ActionResult Create([Bind(Include = "EnrollmentID,InstructorID,GroupID,CID,Grades")] Enrollment enrollment)
         {
             if (ModelState.IsValid)
             {
@@ -83,7 +83,7 @@ namespace Kdg_MVC.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "EnrollmentID,InstructorID,GroupID,ChildID,Grade")] Enrollment enrollment)
+        public ActionResult Edit([Bind(Include = "EnrollmentID,InstructorID,GroupID,CID,Grades")] Enrollment enrollment)
         {
             if (ModelState.IsValid)
             {
